079b964742792912ce9b41475d0bca65e355b306
Mahfuzur Rahman Emon <Emon-MR@ulster.ac.uk>
2026-06-02 13:07:16 +0100

    Add requireAuth utility

--- public/js/app.js
+++ public/js/app.js
@@ -450,3 +450,4 @@ function parseCourses(data) {
 }
 function hideAuthModal() { hideLoginModal(); }
 function escapeHtml(s){return String(s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');}
+function requireAuth(fn) { if (!currentAccount) { showLoginModal(); return; } fn(); }
