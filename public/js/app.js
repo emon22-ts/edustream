10ee4f2e6bf4a35d48329867844cb44d03721871
Mahfuzur Rahman Emon <Emon-MR@ulster.ac.uk>
2026-06-02 13:02:29 +0100

    Add escapeHtml utility

--- public/js/app.js
+++ public/js/app.js
@@ -449,3 +449,4 @@ function parseCourses(data) {
   return [];
 }
 function hideAuthModal() { hideLoginModal(); }
+function escapeHtml(s){return String(s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');}
